231c8f20b14b3ef41c178d3b077658bae2dd8be2
Randy May <randy.may@hazelcast.com>
2023-01-29 18:46:52 -0500

    add encrypted credentials and other updates

--- submitjob.sh
+++ submitjob.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker compose run hazelcast-shell hz-cli submit \
-    -c=com.hzsamples.automl.PredictionPipeline \
+    -c=com.hzsamples.automl.solution.PredictionPipeline \
     -t=hazelcast:5701  \
     -n=fraud-prediction \
     /opt/project/scoring-pipeline/target/scoring-pipeline-1.0-SNAPSHOT.jar  \
     /opt/project/scoring-pipeline/gcp-credentials.json \
-    jet-geo-1572012738362 \
+    hazelcast-33 \
     us-central1 \
-    1485031190799843328
+    6989362321306943488
